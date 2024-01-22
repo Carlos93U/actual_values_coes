8d4929ca89b4944d03f3271d3e09d427bffb398d
carlos <huillcas.juan3@gmail.com>
2024-01-22 15:08:30 -0500

    Final Form to JSON

--- datacoes/datacoes/spiders/scraper.py
+++ datacoes/datacoes/spiders/scraper.py
@@ -2,33 +2,40 @@ import scrapy
 import datetime
 
 class DataCoesSpider(scrapy.Spider):
-    name = 'scrap_coes'
+    name = 'scrap_events'
     start_urls = [
         "https://www.coes.org.pe/Portal/home/"
     ]
     custom_settings = {
-        'FEED_URI': 'data.json',
+        'FEED_URI': 'data_events.json',
         'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUESTS': 5,
+        'MEMUSAGE_LIMIT_MB': 1024,
+        'MEMUSAGE_NOTIFY_MAIL': ["juan.huillcas.a@uni.pe"],
         'ROBOTSTXT_OBEY': True,
         'USER_AGENT': 'user_me',
-        'FEED_EXPORT_ENCODING': 'utf-8'
+        'FEED_EXPORT_ENCODING': "utf-8"
     }
 
     def parse(self, response):
-        events = response.xpath('//div[@class="col-md-6 col-sm-6-2"][1]//h2/a/text()').getall()
+        rel_24h_events = response.xpath('//div[@class="col-md-6 col-sm-6-2"][1]//h2/a/text()').getall()
+        max_dem_events = response.xpath('//div[@class="col-md-6 col-sm-6-2"][2]//h2/a/text()').getall()
         events_out = dict()
-        for i,event in enumerate(events):
+        event_max_out = dict()
+        for i,event in enumerate(rel_24h_events):
             date = response.xpath(f'//div[@class="col-md-6 col-sm-6-2"][1]//div[@class="coes-preview-list"]/div[{i+1}]/p/text()').get()
-            date = [datetime.datetime.now().year,date]
+            #date = [datetime.datetime.now().year,date]
             description = response.xpath(f'//div[@class="col-md-6 col-sm-6-2"][1]//div[@class="coes-preview-list"]/div[{i+1}]/div/p/text()').get()
-            events_out[event] = {'description':description,'date':date}       
-        
+            events_out[event] = {'description':description.replace("\r\n",""),'date':date}       
+                
+        for i,event in enumerate(max_dem_events):
+            description = response.xpath(f'//div[@class="col-md-6 col-sm-6-2"][2]//div[@class="coes-preview-list"]/div[{i+1}]/div/p/text()').get()
+            event_max_out[event.replace("MÁXIMA DEMANDA","")] = {'description':description.replace("\r\n","")} 
+
         yield {
-        'events':events_out
+        'rel_24h_events':events_out,
+        'max_dem_events':event_max_out
         }
-    
-
-
         
         
         
